e0aca1601c4f1e4a3e72ed01147b57d7b40f37f8
silasrex2511 <silasrex2511@gmail.com>
2015-04-18 14:28:48 -0500

    Create FlappingBox.js

---
+++ examples/FlappingBox.js
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Flapping Box</title>
+        <script src="res/keydrown.js" type="text/javascript"></script>
+        <script src="../jsgfl.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <canvas id="theCanvas" width="685" height="600" style="border: 3px solid black"></canvas>
+        <script src="FlappingBox.js" type="text/javascript"></script>
+    </body>
+</html>
